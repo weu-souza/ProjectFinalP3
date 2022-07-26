9b9270d76a85b90cf84a3f9283538b6ec98ac89f
weu-souza <weuller1561@gmail.com>
2022-07-26 04:01:00 -0300

    Commit final

--- src/main/java/Util/Constraints.java
+++ src/main/java/Util/Constraints.java
@@ -1,6 +1,6 @@
 package Util;
 
-import javafx.scene.control.TableView;
+
 import javafx.scene.control.TextField;
 
 public class Constraints {
@@ -8,22 +8,6 @@ public class Constraints {
     public static void setTextFieldInteger(TextField txt) {
         txt.textProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue != null && !newValue.matches("\\d*")) {
-                txt.setText(oldValue);
-            }
-        });
-    }
-
-    public static void setTextFieldMaxLength(TextField txt, int max) {
-        txt.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue != null && newValue.length() > max) {
-                txt.setText(oldValue);
-            }
-        });
-    }
-
-    public static void setTextFieldDouble(TextField txt) {
-        txt.textProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
                 txt.setText(oldValue);
             }
         });
